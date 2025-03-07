94d376e6557c5af481bb8f4b5e0c4b3833a41a93
YuHe123456 <q15yhe@wgsf.net>
2025-03-07 16:00:58 +0000

    Add files via upload

--- books.py
+++ books.py
@@ -9,7 +9,7 @@ class bookFrame(tk.Frame):
         self.parent = parent
         self.titlefont = tkFont.Font(family="Arial", size=20, slant="italic")
         
-        search_type = tk.StringVar()
+        self.search_type = tk.StringVar()
         
         l1 = tk.Label(self,text="Choose Books", font=self.titlefont)
         l1.grid(row=0,column=0)
@@ -18,21 +18,30 @@ class bookFrame(tk.Frame):
         
         
         
-        author_radiobutton = tk.Radiobutton(self,text="Search By Author", value = "author", variable=search_type)
-        title_radiobutton = tk.Radiobutton(self,text="Search By Title", value = "title", variable=search_type)
+        author_radiobutton = tk.Radiobutton(self,text="Search By Author", value = "author", variable=self.search_type)
+        title_radiobutton = tk.Radiobutton(self,text="Search By Title", value = "title", variable=self.search_type)
         
         author_radiobutton.grid(row=1,column=1)
         title_radiobutton.grid(row=1,column=2)
         
-        search_box = tk.Entry(self)
-        search_box.grid(row=1,column=0)
+        self.search_box = tk.Entry(self) 
+        self.search_box.grid(row=1,column=0)
         
-        search_button = tk.Button(self,text="Search")
-        search_button.grid(row=1,column=3)
+        self.books_listbox = tk.Listbox(self)
+        self.books_listbox.grid(row=2,column=0)
         
-        books_listbox = tk.Listbox(self)
-        books_listbox.grid(row=2,column=0)
+        self.search_button = tk.Button(self,command=self.update_book_list)
+        self.grid(row=1,column=2)
         
+    def update_book_list(self):
+        
+        search_term = self.search_box.get()
+        
+        search_result_list = self.parent.cursor.execute("SElECT * FROM books WHERE ? LIKE '%||?||%'", (self.search_type,search_term)).fetchall()
+        
+        
+    
+    
         
         
 
